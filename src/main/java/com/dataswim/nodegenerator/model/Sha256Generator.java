64b7cdc6245f1fc394574e78aa2c6b76be243aa4
georgy.saukov <gsaukov@yahoo.com>
2021-06-13 01:19:56 +0200

    sha 256 generator

---
+++ src/main/java/com/dataswim/nodegenerator/model/Sha256Generator.java
@@ -0,0 +1,15 @@
+package com.dataswim.nodegenerator.model;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+public class Sha256Generator {
+
+    public static String getSha256(String text) throws NoSuchAlgorithmException {
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte[] hash = digest.digest(text.getBytes(StandardCharsets.UTF_8));
+        return Base64.getEncoder().encodeToString(hash);
+    }
+}
