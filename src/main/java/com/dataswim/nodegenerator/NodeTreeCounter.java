cdec98b7bfd73fa2f5f1a7f293dbac78490b8867
georgy.saukov <gsaukov@yahoo.com>
2021-06-26 00:14:35 +0200

    Node tree generation

---
+++ src/main/java/com/dataswim/nodegenerator/NodeTreeCounter.java
@@ -0,0 +1,20 @@
+package com.dataswim.nodegenerator;
+
+import com.dataswim.nodegenerator.model.Node;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class NodeTreeCounter {
+
+    public int count(List<Node> roots) {
+        int counter = 0;
+        if(!roots.isEmpty())
+            for(Node root : roots) {
+                counter += (count(root.getChilds()) + 1);
+            }
+        return counter;
+    }
+}
