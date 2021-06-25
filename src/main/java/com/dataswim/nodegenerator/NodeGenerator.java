baab4d979e82812057e832fac2da8bc499aa45ae
georgy.saukov <gsaukov@yahoo.com>
2021-06-25 23:55:50 +0200

    Node tree generation

--- src/main/java/com/dataswim/nodegenerator/NodeGenerator.java
+++ src/main/java/com/dataswim/nodegenerator/NodeGenerator.java
@@ -1,11 +1,16 @@
 package com.dataswim.nodegenerator;
 
+import java.util.UUID;
+import com.dataswim.nodegenerator.model.Node;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class NodeGenerator {
 
-    public void generate() {
+    public Node generate(Node parent, int childCount, int depth) {
+        Node node = new Node(Integer.toString(depth), parent);
 
+        return node;
     }
 }
