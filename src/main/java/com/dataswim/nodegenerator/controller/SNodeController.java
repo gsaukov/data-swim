ea05f17326d36d4346b70603fbf10a40dc729a8a
georgy.saukov <gsaukov@yahoo.com>
2021-06-30 18:00:29 +0200

    contoller

---
+++ src/main/java/com/dataswim/nodegenerator/controller/SNodeController.java
@@ -0,0 +1,25 @@
+package com.dataswim.nodegenerator.controller;
+
+import com.dataswim.nodegenerator.model.SNode;
+import com.dataswim.nodegenerator.persistence.SNodeRepository;
+import com.dataswim.nodegenerator.services.SNodeGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class SNodeController {
+
+    @Autowired
+    SNodeRepository repository;
+
+    @GetMapping("/v1/populate")
+    public void populate() {
+        SNodeGenerator generator = new SNodeGenerator();
+        SNode n = generator.generate(null, 3, 6);
+        repository.save(n);
+    }
+
+}
