0a718efea01a832a6d9352774aa5b0ec646f5bad
georgy.saukov <gsaukov@yahoo.com>
2021-09-26 23:33:01 +0200

    postgress db liquibase attachement

--- src/main/java/com/dataswim/nodegenerator/batch/JobCompletionListener.java
+++ src/main/java/com/dataswim/nodegenerator/batch/JobCompletionListener.java
@@ -1,15 +1,17 @@
 package com.dataswim.nodegenerator.batch;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.listener.JobExecutionListenerSupport;
 
+@Slf4j
 public class JobCompletionListener extends JobExecutionListenerSupport {
 
     @Override
     public void afterJob(JobExecution jobExecution) {
         if (jobExecution.getStatus() == BatchStatus.COMPLETED) {
-            System.out.println("BATCH JOB COMPLETED SUCCESSFULLY");
+            log.info("BATCH JOB COMPLETED SUCCESSFULLY");
         }
     }
 
